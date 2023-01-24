82c6a36eba5b1333388bb0dc502406daea7e9211
Dmitriy Metelkov <d.metelkov@mail.ru>
2023-01-24 17:29:15 +0300

    first apply

--- main.tf
+++ main.tf
@@ -12,3 +12,17 @@ variable "do_token" {}
 provider "digitalocean" {
   token = var.do_token
 }
+
+resource "digitalocean_droplet" "web1" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-1"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
+
+resource "digitalocean_droplet" "web2" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-2"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
